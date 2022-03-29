2ac6221b310729c2b2745aae473a6716ec7fc262
Mojtaba Moradli <99252405+mojtabamoradli@users.noreply.github.com>
2022-03-29 18:32:23 +0430

    Update routes.php

--- routes.php
+++ routes.php
@@ -2,9 +2,10 @@
 	if(isset($_GET['route'])) {
 		$route = $_GET['route'];
 
-		if($route == "home") { echo json_encode(array( 'url' => '', 'data'=>file_get_contents('./index.php'), )); }
-		else if($route == "page1") { echo json_encode(array( 'url' => $route, 'data'=>file_get_contents('./page1.php'), )); }
-		else if($route == "page2") { echo json_encode(array( 'url' => $route, 'data'=>file_get_contents('./page2.php'), )); }
-		else if($route == "page3") { echo json_encode(array( 'url' => $route, 'data'=>file_get_contents('./page3.php'),)); } 
+		if($route == "home") { echo json_encode(array('status'=>'success', 'title'=> 'Home', 'url' => '', 'data'=>file_get_contents('./index.php'), )); }
+		else if($route == "page1") { echo json_encode(array('status'=>'success', 'title'=> 'Page 1', 'url' => $route, 'data'=>file_get_contents('./page1.php'), )); }
+		else if($route == "page2") { echo json_encode(array('status'=>'success', 'title'=> 'Page 2' , 'url' => $route, 'data'=>file_get_contents('./page2.php'), )); }
+		else if($route == "page3") { echo json_encode(array('status'=>'success', 'title'=> 'Page 3' , 'url' => $route, 'data'=>file_get_contents('./page3.php'),)); } 
 }
+
 
